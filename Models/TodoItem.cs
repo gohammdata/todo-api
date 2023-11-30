dc8e6f88ae6467a9ac97ae76258f2a8ab2a62767
John <gohammdata@gmail.com>
2023-11-30 18:43:11 -0500

    mongo -> model

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,8 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace TodoApi.Models;
 
 public class TodoItemDTO
 {
-    public long Id {get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id {get; set; }
+    [BsonElement("Name")]
     public string? Name { get; set; }
     public bool IsComplete {get; set; }
 }
--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c5d261c9f9f6a499cb5afd0eaf1949658c5ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
